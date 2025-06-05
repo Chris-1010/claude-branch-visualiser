32a41568a5468e1a8c0a72d1420f5221649bc965
Christopher Ahern <122332721@umail.ucc.ie>
2025-06-05 17:40:57 +0200

    Base Project

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { useState } from "react"
+import "./App.css"
+import ChatTree from "./components/ChatTree"
+
+function App() {
+  
+  return (
+    <>
+      <div className="App">
+        <header className="App-header">
+          <h1>Claude Branch Visualiser</h1>
+        </header>
+        <main>
+        </main>
+      </div>
+    </>
+  )
+}
+
+export default App
