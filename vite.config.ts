2e6a3233699b03b46b4242dd0e4564b4a6dac7b7
Christopher Ahern <122332721@umail.ucc.ie>
2025-07-29 11:26:29 +0200

    Containerize Application

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // Docker container port mapping
+    port: 5175,
+    watch: {
+      usePolling: true, // File watching in Docker
+    },
+  },
+  preview: {
+    host: true,
+    port: 3005,
+  }
 })
